a5d7ee58d6759ddb4eb388e67a15dfcd7641a440
hodgeslo <895933+hodgeslo@users.noreply.github.com>
2023-01-25 10:57:04 -0500

    It seems so simple now
    It seems so simple now. The display() was giving me all the trouble for several days.  Time to clean up the code.

--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -17,6 +17,7 @@ class Game:
         guesses: This is a list that contains the letters guessed by the user.
         """
         self.missed = 0
+        self.max_num_of_attempts = 5
         self.phrases = [
             "JAWS OF DEATH",
             "HANDS DOWN",
@@ -44,54 +45,58 @@ class Game:
     """
 
     def welcome(self):
-        print(f"WELCOME TO PHRASE HUNTER")
+        print(r"""
+
+  _____  _                          _    _             _              _____           _     _____ _____ 
+ |  __ \| |                        | |  | |           | |            |  __ \         | |   |_   _|_   _|
+ | |__) | |__  _ __ __ _ ___  ___  | |__| |_   _ _ __ | |_ ___ _ __  | |__) |_ _ _ __| |_    | |   | |  
+ |  ___/| '_ \| '__/ _` / __|/ _ \ |  __  | | | | '_ \| __/ _ \ '__| |  ___/ _` | '__| __|   | |   | |  
+ | |    | | | | | | (_| \__ \  __/ | |  | | |_| | | | | ||  __/ |    | |  | (_| | |  | |_   _| |_ _| |_ 
+ |_|    |_| |_|_|  \__,_|___/\___| |_|  |_|\__,_|_| |_|\__\___|_|    |_|   \__,_|_|   \__| |_____|_____|
+        """)
 
     """
     get_guess(): this method gets the guess from a user and records it in the guesses attribute
     """
 
     def get_guess(self):
-        print("\nGET GUESS")
+        print(f"from get_guess(): {self.active_phrase}")
+        if not self.guesses:
+            for key, value in enumerate(self.active_phrase):
+                if value.isalpha():
+                    print("_", end=" ")
+                else:
+                    print(" ", end="")
 
-        # if not self.guesses:
-        #     # print(f"NOT EMPTY:  self.guesses []")
-        #     for key, value in enumerate(self.active_phrase):
-        #         # print(key, value)
-        #         if value.isalpha():
-        #             value = "_"
-        #             print("_", end=" ")
-        #             # dashed_list.append(value)
-        #             # print(f"TEMP LIST: {temp_list}")
-        #         else:
-        #             value = " "
-        #             print("  ", end="")
-        #             # dashed_list.append(value)
+        user_guess = input("\nGuess a letter:  ").lower()
 
-        user_guess = input("\n\nGuess a letter:  ").lower()
-
-        if user_guess == "" or not user_guess.isalpha() or len(user_guess) > 1 or [idx for idx, element in enumerate(self.guesses) if element == user_guess]:
-            print(f"Invalid entry or you already guessed that letter. Enter a letter and ensure that it is not a duplicate guess.")
+        if user_guess == "" or not user_guess.isalpha():
+            print(f"*** Invalid entry. Enter one letter only between A and Z. ***")
+        elif len(user_guess) > 1:
+            print(f"*** Too many letters entered. Enter one letter only between A and Z. Try again! ***")
+        elif [idx for idx, element in enumerate(self.guesses) if element == user_guess]:
+            print(f"*** You already guessed that letter. Try again! ***")
         else:
             if self.active_phrase.check_letter(user_guess) is False:
-                print(f"FALSE FOOL")
                 self.missed += 1
-                print(self.missed)
-                # self.active_phrase.display()
+                print(f"You have {self.max_num_of_attempts - self.missed} out of {self.max_num_of_attempts} lives remaining!")
             else:
-                print(f"TRUE TRAMP")
                 self.guesses.append(user_guess)
-                # print(f"from true tramp:  {self.active_phrase.display(user_guess)}")
-                # self.active_phrase.display(user_guess)
-                # self.active_phrase.display(self.guesses)
-                self.active_phrase.display(user_guess, self.guesses)
-                # print(f"from true tramp:  {self.active_phrase.display(self.guesses)}")
-        # print(f"user guesses matched:  {self.guesses}")
+                self.active_phrase.display(self.guesses)
 
     """
     game_over(): this method displays a friendly win or loss message and ends the game.
     """
 
     def game_over(self):
+        if self.missed == self.max_num_of_attempts:
+            print("you lost!")
+            try:
+                user_play_again = input("Would you like to play again? y/n  ")
+                if user_play_again == " " or not user_play_again.isalpha():
+                    print(f"Oops, try again")
+            except ValueError:
+                print(f"Oops, try again")
         return f"GAME OVER"
 
     """
@@ -103,19 +108,11 @@ class Game:
         # display welcome message to player
         self.welcome()
 
-        # print(f"from start() with initial value: {self.active_phrase} and {id(self.active_phrase)}")
+        print(f"from start() with initial value: {self.active_phrase} and {id(self.active_phrase)}")
 
         self.active_phrase = phrase.Phrase(self.get_random_phrase())
 
-        print(f"From start(): {self.active_phrase} and {id(self.active_phrase)}")  # remove later
-
-        # for key, value in enumerate(self.active_phrase):
-        #     if value.isalpha():
-        #         print("_", end=" ")
-        #     else:
-        #         print("  ", end="")
-
-        while self.missed < 5:
+        while self.missed < self.max_num_of_attempts:
             self.get_guess()
         else:
             print(self.game_over())
--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -12,105 +12,34 @@ class Phrase:
     def __iter__(self):
         return iter(self.active_phrase)
 
-    def __repr__(self):
-        return self
-
-    def find_indices(self, list_to_check, item_to_find):
-        indices = []
-        for idx, value in enumerate(list_to_check):
-            if value == item_to_find:
-                indices.append(idx)
-        return indices
-
-
     """
      TODO: display(): this prints out the phrase to the console with guessed letters visible and unguessed letters as 
      underscores. For example, if the current phrase is "hello world" and the user has guessed the letter "o", 
      the output should look like this: _ _ _ _ o   _ o _ _ _ 
     """
 
-    def display(self, user_guess, user_guessed_list):
-        # active_phrase_list = list(self.active_phrase)
-        # print(f"from phrase.display():  {self.active_phrase} and {id(self.active_phrase)}")
-        print(f"from phrase.display() WHAT: {user_guessed_list}")
+    def display(self, user_guessed_list):
+        # print(f"from phrase.display() WHAT: {user_guessed_list}")
 
-        print(f"\nACTIVE PHRASE LIST: {list(self.active_phrase)}")
-
-        #the_indices = [index for (index, item) in enumerate(self.active_phrase) if item == user_guess]
-        #print(f"THE INDICES: {the_indices}")
+        # print(f"\nACTIVE PHRASE LIST: {list(self.active_phrase)}")
 
         dashed_list = []
 
         for key, value in enumerate(iter(self.active_phrase)):
-            # print(key, value)
             if value.isalpha():
                 value = "_"
-                # print("_", end=" ")
                 dashed_list.append(value)
             else:
                 value = " "
-                # print("  ", end="")
                 dashed_list.append(value)
 
-        print(f"\ndash list!: {' '.join(dashed_list)}  ")
-
-        print(f"NEW DASHED LIST: {dashed_list}")
-
         for a, b in enumerate(user_guessed_list):
-            #print(a, b)
-            #print([index for (index, item) in enumerate(self.active_phrase) if item == b])
             for idx, itm in enumerate(self.active_phrase):
                 if itm == b:
-                    print(itm)
                     dashed_list[idx] = itm
-        print(f"\ndash list!: {''.join(dashed_list)}  ")
+        print(f"\n {' '.join(dashed_list)}")
 
-
-        #
-        # the_indices = []
-        # for idx, value in enumerate(self.active_phrase_list):
-        #     if value == item_to_find:
-        #         the_indices.append(idx)
-
-
-
-
-
-        """ WORK ABOVE THIS LINE """
-
-        # for key, value in enumerate(temp_list):
-        #     # print(key, value)
-        #     if value.isalpha():
-        #         value = "_"
-        #         # print("_", end=" ")
-        #         dashed_list.append(value)
-        #         # print(f"TEMP LIST: {temp_list}")
-        #     else:
-        #         value = " "
-        #         # print("  ", end="")
-        #         dashed_list.append(value)
-
-        # print(f"dash list!: {' '.join(dashed_list)}  ")
-
-        # the_indices = [idx for idx, element in enumerate(temp_list) if element == user_guessed_list]
-        # print(f"from display() the indices: {the_indices} and {type(the_indices)}")
-        # (print("\n\n"))
-        #
-        # for i, k in enumerate(the_indices):
-        #     dashed_list[k] = user_guessed_list
-        #
-        # my_list = dashed_list.copy()
-        # print(f"MY LIST:  {my_list}")
-        #
-        # print(f"updated dash list!: {' '.join(dashed_list)}  ")
-        #
-        # print("\nWILL THIS WORK????")
-        # for x, y in enumerate(user_guessed_list):
-        #     print(x, y)
-        #     print([idx1 for idx1, element1 in enumerate(temp_list) if element1 == user_guessed_list[x]])
-
-        # return dashed_list
-
+        self.check_complete(dashed_list)
 
     """
     TODO: checks to see if the letter selected by the user matches a letter in the phrase.
@@ -127,4 +56,8 @@ class Phrase:
     """
 
     def check_complete(self, user_guessed_list):
-        pass
+        if user_guessed_list.count("_") > 0:
+            print("game in progress")
+        else:
+            print(f"you win!")
+
